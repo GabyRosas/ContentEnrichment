e9a6de032c6206952f17b4750d538698bed36fc4
GabyRosas <fleuryrosas@gmail.com>
2024-07-16 13:00:00 +0200

    function scraper OK with only P

--- src/main.py
+++ src/main.py
@@ -1,3 +1,15 @@
-# hola
+from src.emotion import Emotion
+
+joy = Emotion("Joy", 13, "mak fun", 10, "yellow")
+
+print(joy.name)
+print(joy.get_color())
+print(joy.introduce())
+
+sadness = Emotion("Sadness", 13, "blue")
+
+print(sadness.introduce())
+
+
 
 
--- src/requests_wiki.py
+++ src/requests_wiki.py
@@ -12,16 +12,6 @@ url = "https://es.wikipedia.org/wiki/"+word
 def wiki_search(url):
     url_open = requests.get(url)
     soup = BeautifulSoup(url_open.content, 'html.parser')
-    details = soup.find_all('table',{'class':'infobox'})
-    for i in details:
-        h = i.find_all('tr')
-        for j in h:
-            heading = j.find_all('th')
-            detail = j.find_all('td')
-            if heading is not None and detail is not None:
-                for x,y in zip(heading,detail):
-                    print("{}  ::  {}".format(x.text, y.text))
-                    print("------------------")
     paragraphs = soup.find_all('p')
     for i in range(min(5, len(paragraphs))):
         print(paragraphs[i].text)
