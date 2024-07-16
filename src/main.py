78b3bcab0defa7af39b8b2ad8a529eb30f8a1510
helopgom <helena.lopez.gomez@gmail.com>
2024-07-16 13:13:25 +0200

    Change document. Add format to paragraphs

--- src/main.py
+++ src/main.py
@@ -1,14 +1,4 @@
-from src.emotion import Emotion
 
-joy = Emotion("Joy", 13, "mak fun", 10, "yellow")
-
-print(joy.name)
-print(joy.get_color())
-print(joy.introduce())
-
-sadness = Emotion("Sadness", 13, "blue")
-
-print(sadness.introduce())
 
 
 
