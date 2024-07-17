09e5ba2a50ddfea76f5caae0a808e075a4f17dbf
helopgom <helena.lopez.gomez@gmail.com>
2024-07-17 14:10:51 +0200

    Add test to ScraperService

---
+++ tests/test_scraper_service.py
@@ -0,0 +1,28 @@
+import pytest
+from src.ScraperService import ScraperService
+
+
+
+@pytest.fixture
+def scraper_service():
+    return ScraperService()
+
+
+
+def test_valid_search(scraper_service, monkeypatch):
+
+    monkeypatch.setenv('WIKI_URL_BASE', 'https://es.wikipedia.org/wiki/')
+
+    search_query = "Python"
+    paragraphs = scraper_service.get_wiki_content(search_query)
+
+    assert len(paragraphs) > 0, "No se obtuvieron párrafos para una búsqueda válida"
+
+def test_invalid_search(scraper_service, monkeypatch):
+    # Simulamos el valor de WIKI_URL_BASE en el entorno de prueba
+    monkeypatch.setenv('WIKI_URL_BASE', 'https://es.wikipedia.org/wiki/')
+
+    search_query = "fgjdfgjdfgjdfgjdfgj"
+    paragraphs = scraper_service.get_wiki_content(search_query)
+
+    assert len(paragraphs) == 0, "Se obtuvieron párrafos para una búsqueda inválida"
