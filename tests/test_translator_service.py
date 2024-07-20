4fb416768948256ed23f27f664fc85a0e90f8a9f
Angelica perez <angelicanakaridperezpino@gmail.com>
2024-07-20 21:38:47 +0200

    finish test

--- tests/test_translator_service.py
+++ tests/test_translator_service.py
@@ -1,47 +1,24 @@
 import pytest
 from utils.TranslatorText import TranslatorText
 
-
-def mock_get_user_input(prompt):
-    responses = {
-        'Ingresa un texto: ': 'hola',
-        'Tu lenguaje: ': 'es',
-        'Lenguaje deseado: ': 'en'
-    }
-    return responses[prompt]
-
-
-def test_translation(monkeypatch, capsys):
-    # Usar monkeypatch para reemplazar get_user_input con nuestra función simulada.
-    monkeypatch.setattr('translator.get_user_input', mock_get_user_input)
-
-    # Crear una instancia de TranslatorText y ejecutar el método main.
+def test_invalid_language():
     translator = TranslatorText()
-    translator.main()
+    text_input = 'Hola mundo'
+    src_lang_input = 'es'
+    tgt_lang_input = 'xx'  # Lenguaje incorrecto
 
-    # Capturar la salida estándar.
-    captured = capsys.readouterr()
+    result = translator.main(text_input, src_lang_input, tgt_lang_input)
 
-    # Verificar que la salida es la esperada.
-    assert captured.out.strip() == 'hello'
+    assert result == "Translation not found" or "An unexpected error was found" in result
 
 
-def test_translation_language_error(monkeypatch, capsys):
-    # Usar monkeypatch para reemplazar get_user_input con nuestra función simulada.
-    # Esta vez simula un error en el idioma de origen.
-    responses = {
-        'Ingresa un texto: ': 'hola',
-        'Tu lenguaje: ': 'xx',  # Idioma inexistente o incorrecto.
-        'Lenguaje deseado: ': 'en'
-    }
-    monkeypatch.setattr('translator.get_user_input', lambda prompt: responses[prompt])
-
-    # Crear una instancia de TranslatorText y ejecutar el método main.
+def test_valid_translation():
     translator = TranslatorText()
-    translator.main()
+    text_input = 'Hola mundo'
+    src_lang_input = 'es'
+    tgt_lang_input = 'en'  # Lenguaje correcto
 
-    # Capturar la salida estándar.
-    captured = capsys.readouterr()
+    result = translator.main(text_input, src_lang_input, tgt_lang_input)
 
-    # Verificar que la salida es la esperada para un error de idioma.
-    assert captured.out.strip() == 'Translation not found'
+    # Verificar que la traducción sea la esperada
+    assert result == 'Hello World'
